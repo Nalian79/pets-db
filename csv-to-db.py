7a21b992f53b3501f01e4eeef1bb5d079face816
Michele <michele.morelock@gmail.com>
2015-01-04 21:15:25 +0000

    Able to add names to pets from csv
    Got the db connection working and able to add the name value
    to the pets table.  Need to sort out uniqueness, etc.

--- csv-to-db.py
+++ csv-to-db.py
@@ -1,6 +1,6 @@
 import csv
 import logging
-#import psycopg2
+import psycopg2
 import sys
 
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
@@ -8,28 +8,52 @@ logging.basicConfig(filename="output.log", level=logging.DEBUG)
 # Open and parse CSV file, create dict from what we find.
 
 def readcsv(filename):
-    """ Read in a CSV file and create a dict out of the data """
+    """ Read in a CSV file and create a dict out of the data.
+    Return this dict as a list for easy access later. Ensure that
+    values are lowercase. """
     logging.info("Reading in {!r}".format(filename))
+    pet_info = []
     with open(filename, "r+") as f:
         logging.debug("Opened file {!r} successfully.".format(filename))
         reader = csv.DictReader(f, delimiter=',')
-        pet_info = []
         for row in reader:
             pet_info.append(row)
-            logging.debug("Inserted contents into dict: {!r}".format(row))
-        return pet_info
+            logging.debug("Inserted {!r} into list".format(row))
+    return pet_info
+
+def add_pets(pet_list, database="pets"):
+    """Add pets to our database from the csv file we parsed into a list """
+
+    logging.debug("Trying database connection.")
+    try:
+        conn=psycopg2.connect(database=database)
+    except:
+        print "Couldn't connect to the DB!"
+
+    cur=conn.cursor()
+
+    for item in pet_list:
+        logging.debug("Trying to insert {!r}".format(item))
+        query = "INSERT INTO pet (name) VALUES ({!r})".format(
+            item['Name'])
+        cur.executemany(query, item)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 def main():
-    """ Main function
-    Changing purpose as the script grows...
-    """
+    """ Main function """
     logging.info("Starting script..")
     # Lets make sure we can read in the CSV file and get a valid dict.
     file_name="pets_to_add.csv"
+    # Create list of pet info in dict format
     pet_list = readcsv(file_name)
-    logging.info("Trying to read in {!r}.".format(file_name))
-    for item in pet_list:
-        print item
+
+    # Add pets to the db
+    add_pets(pet_list)
+
 
 
 if __name__ == "__main__":
